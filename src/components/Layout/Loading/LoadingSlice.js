a93375fbb45f79bb26ba9ea396b64ef89fdb7861
Traczyk <96traczyklukasz@gmail.com>
2022-09-10 06:08:12 +0200

    fixed the loading slice

--- src/components/Layout/Loading/LoadingSlice.js
+++ src/components/Layout/Loading/LoadingSlice.js
@@ -2,7 +2,7 @@ import { createSlice } from '@reduxjs/toolkit';
 
 const LoadingSlice = createSlice({
     initialState: {
-        loading: true
+        loading: false
     },
     name: 'loading',
     reducers: {
